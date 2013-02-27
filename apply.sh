c0e9456d17fd1db649c0398c8eff09de61b57779
Hrubak <hrubak@gmail.com>
2013-02-27 11:19:15 -0500

    derp

--- apply.sh
+++ apply.sh
@@ -93,7 +93,7 @@ git reset --hard
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Gallery2 refs/changes/54/29154/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto packages/apps/Mmms
+repo start auto packages/apps/Mms
 echo "Add quick emoji button next to text input"
 cdv packages/apps/Mms
 git reset --hard
