f90f6d98f475ae65406aa964bdc6521fdbe6ce31
Hrubak <hrubak@gmail.com>
2013-04-01 09:41:09 -0300

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -87,6 +87,13 @@ git reset --hard
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_kernel_samsung_d2 refs/changes/10/34910/1 && git cherry-pick FETCH_HEAD
 cdb
 
+repo start auto kernel/samsung/d2
+echo "bcmdhd: squash updated bcmdhd stack from google"
+cdv kernel/samsung/d2
+git reset --hard
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_kernel_samsung_d2 refs/changes/28/34928/1 && git cherry-pick FETCH_HEAD
+cdb
+
 repo start auto device/samsung/d2-common
 echo "d2: Update Adreno blob list"
 cdv device/samsung/d2-common
